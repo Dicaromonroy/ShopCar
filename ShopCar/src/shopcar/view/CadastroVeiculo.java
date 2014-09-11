3febc06f7cc1bd84172904f3b303d892227e06e0
flepxx9 <vitor_freitas_b@hotmail.com>
2014-09-11 17:28:41 -0300

    Ajustes

--- ShopCar/src/shopcar/view/CadastroVeiculo.java
+++ ShopCar/src/shopcar/view/CadastroVeiculo.java
@@ -10,6 +10,8 @@ import java.lang.reflect.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
 import shopcar.controller.Validator;
 import shopcar.model.*;
@@ -66,36 +68,47 @@ public class CadastroVeiculo
         System.out.println("");
         load();
         veiculo = testIfNeedToListVeiculos();
+        if(veiculo == null) 
+        {
+            System.err.println("Não existe esse Tipo de Veiculo!");
+            testIfNeedToListVeiculos();
+        }
         
         try
         {
-            inputMaker(veiculo, "Entre com a Placa do Veiculo: ", "Placa"); 
-            System.err.println(veiculo.getPlaca());
+            inputMaker(veiculo, "Entre com a Placa do Veiculo: ", "Placa", String.class, 1); 
+            System.out.println(veiculo.getPlaca());
         } 
-        catch(IllegalAccessException ex)
+        catch(Exception ex)
         {
-            System.err.println("Ilegal");
-        } catch (IllegalArgumentException ex)
-        {
-            System.err.println("argument");
-        } catch (NoSuchMethodException ex)
-        {
-            System.err.println("method");
-        } catch (InvocationTargetException ex)
-        {
-            System.err.println("invocation");
+            System.err.println(ex.getCause());
+            System.err.println(ex.getMessage());
+            System.err.println(ex.getLocalizedMessage());
         }
     }
 
-    public void inputMaker(Veiculo obj, String string, String property) throws NoSuchMethodException, IllegalAccessException, 
+    public void inputMaker(Veiculo obj, String question, String property, Class propertyClass, Integer methodInheritanceHierarchy) throws  IllegalAccessException, 
             IllegalArgumentException, InvocationTargetException
     {
         String err;
-        System.out.println(string);
+        System.out.println(question);
         String test = sCad.nextLine();
-        Method method = obj.getClass().getSuperclass().getDeclaredMethod("set" + property, Object.class);
-        method.invoke(obj, test);
-        if(!testSaveInput(obj, property.toLowerCase())) inputMaker(obj, string, property);
+        
+        for(int i = 1; i <= methodInheritanceHierarchy; i++) 
+        {
+            
+        }
+        try
+        {
+            Method method = obj.getClass().getSuperclass().getSuperclass().getDeclaredMethod("set" + property, propertyClass);
+            method.invoke(obj, test);
+        } 
+        catch (NoSuchMethodException e)
+        {
+            System.err.println(e.getCause());
+        }
+        
+        if(!testSaveInput(obj, property.toLowerCase())) inputMaker(obj, question, property, propertyClass, methodInheritanceHierarchy);
     }
     
     public boolean testSaveInput(Veiculo obj, String property)
@@ -108,6 +121,11 @@ public class CadastroVeiculo
         }
         else
             return true;
+    }
+    
+    public Object getSuperClass(Object obj)
+    {
+        return obj.getClass().getSuperclass();
     }
     
     public Veiculo testIfNeedToListVeiculos()
